ba016b71b2aabe62cdc57e3dc99b4cc8ca78b091
Galen <gmitt98@users.noreply.github.com>
2023-05-18 19:01:50 -0700

    update to main

--- Starter_Code/import pkg_resources.py
+++
@@ -1,8 +0,0 @@
-import pkg_resources
-
-# Get a list of all installed modules and their versions
-installed_packages = pkg_resources.working_set
-
-# Print the name and version of each installed module
-for package in installed_packages:
-    print(package)
